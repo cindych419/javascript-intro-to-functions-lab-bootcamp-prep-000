7aef3447bf448a36cabc7a801f45062ff35b8588
cindych419@gmail.com <cindych419@gmail.com>
2018-02-22 07:26:02 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ function whisper(string) {return string.toLowerCase()}
 function shout(string) {return string.toUpperCase()}
 function logShout(string) {console.log (`HELLO`)}
 function logWhisper(string) {console.log (`hello`)}
-function sayHiToGrandma(string) {return "I love you, too."}
+
 
 
 
